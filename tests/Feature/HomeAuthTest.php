908027df321be07d789632f855a274c179644236
Jeppevinkel <jeppevinkel@gmail.com>
2020-03-23 18:04:25 +0100

    Added a couple unit tests
    Signed-off-by: Jeppevinkel <jeppevinkel@gmail.com>

---
+++ tests/Feature/HomeAuthTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeAuthTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->withSession()->get('/home');
+
+        $response->assertStatus(200);
+    }
+}
